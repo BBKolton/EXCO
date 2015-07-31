acabc930f501e00b403ae1d144e6fc506151604a
BBKolton <bbkolton@uw.edu>
2015-07-31 13:59:48 -0700

    Added Refusal of Bad Class Signups
    Users can no longer request to be in a class that does not exist, or a
    section that is closed

--- public_html/signupsubmit.php
+++ public_html/signupsubmit.php
@@ -7,12 +7,18 @@
 	//redirect user with no login
 	if (empty($_SESSION["name"])) {
 		header("Location: login.php");
-		die();
 	}
 
+	//get info from teh class and section requested
+	$db = new DB();
+	$section = $db -> select("SELECT * FROM " . $DATABASE . ".sections
+	                         WHERE course_id = " . $db->quote($_GET["id"]) . 
+	                       " AND section = " . $db->quote($_GET["section"]));
+
 	//bad data sent? send them to the bad person zone! also die.
-	if (empty($_GET["id"]) || empty($_GET["section"])) {
-		error("Unknown Signup", "You did not specify a section or class to signup for!");
+	if (empty($_GET["id"]) || empty($_GET["section"]) ||
+	    empty($section[0]) || $section[0]['status'] !== "1") {
+		error("Unknown Signup", "You did not specify a valid section or class to signup for, or the class is closed!");
 	}
 
 	//If first item in cart, make a new array and put the class in there
